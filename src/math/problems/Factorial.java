4e6bf892746479b350d6ad132f05d08eeb00af4b
syedschowdhury <mchowdhury179@gmail.com>
2020-07-05 21:21:49 -0400

    math

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Scanner;
+
 /**
  * Created by mrahman on 04/02/18.
  */
@@ -11,6 +13,18 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        Scanner input = new Scanner(System.in);
+        int num, fact = 1;
+        System.out.print("Enter any positive integer: ");
+        num = input.nextInt();
+
+        for (int i = num; i >=1; i--) {
+            fact = fact * i;
+
+
+        }
+        System.out.println("Factorial of "+fact);
+
 
     }
 }
